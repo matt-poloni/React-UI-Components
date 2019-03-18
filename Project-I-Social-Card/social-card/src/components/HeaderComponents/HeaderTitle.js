22f29948c3cacd4f8ba5284bd7cf2f0a3d0f3593
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-18 16:11:52 -0400

    Sets up HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './Header.css';
 
 const HeaderTitle = () => {
-  return ();
+  return (
+    <div className="wrap-header-title">
+      <span className="header-name">Lambda School</span>
+      <span className="header-handle">@LambdaSchool</span>
+      <span className="header-date">26 jan</span>
+    </div>
+  );
 }
 
 export default HeaderTitle;
