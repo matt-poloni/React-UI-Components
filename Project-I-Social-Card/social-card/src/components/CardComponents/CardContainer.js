fb573240d121ed18daaf690fbe3277da5507a4b2
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-18 16:18:57 -0400

    Fixes imports on containers

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
-import 'CardBanner' from './CardBanner.js';
-import 'CardContent' from './CardContent.js';
+import CardBanner from './CardBanner.js';
+import CardContent from './CardContent.js';
 
 const CardContainer = () => {
   return ();
