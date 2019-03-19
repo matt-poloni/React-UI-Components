89923630d5f368e919238fe22500dea7457d1a49
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-19 18:42:38 -0400

    Sets up handlers that should work, buttons still need tie-ins

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -15,11 +15,35 @@ class App extends React.Component {
     };
   }
 
+  // Clear Handler
+  handleClear() { this.setState({ total: '' }) }
+
+  // Number Handlers
+  handleOne() { this.setState({ total: this.state.total + '1' }) }
+  handleTwo() { this.setState({ total: this.state.total + '2' }) }
+  handleThree() { this.setState({ total: this.state.total + '3' }) }
+  handleFour() { this.setState({ total: this.state.total + '4' }) }
+  handleFive() { this.setState({ total: this.state.total + '5' }) }
+  handleSix() { this.setState({ total: this.state.total + '6' }) }
+  handleSeven() { this.setState({ total: this.state.total + '7' }) }
+  handleEight() { this.setState({ total: this.state.total + '8' }) }
+  handleNine() { this.setState({ total: this.state.total + '9' }) }
+  handleZero() { this.setState({ total: this.state.total + '0' }) }
+
+  // Operator Handlers
+  handleDivide() { this.setState({ total: this.state.total + '/' }) }
+  handleMultiply() { this.setState({ total: this.state.total + '*' }) }
+  handleSubtract() { this.setState({ total: this.state.total + '-' }) }
+  handleAdd() { this.setState({ total: this.state.total + '+' }) }
+
+  // Equals Handler
+  handleEquals() { this.setState({ total: eval(this.state.total) }) }
+
   render() {
     return (
       <div className="calculator">
         <CalculatorDisplay total={this.state.total} />
-        <ActionButton text='clear' buttonStyle='' />
+        <ActionButton text='clear' buttonStyle='' on/>
         <NumberButton text='÷' buttonStyle='operator' />
         <NumberButton text='7' buttonStyle='number' />
         <NumberButton text='8' buttonStyle='number' />
